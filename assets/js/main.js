32eb3a666d52eae3c05b2308c39324af416ea11a
Socrates <socrates.sotos@gmail.com>
2017-05-05 15:13:54 -0400

    found libraries and started implementing
    found libraries to add visual appeal

--- assets/js/main.js
+++ assets/js/main.js
@@ -378,6 +378,22 @@ var Main = new Vue ({
 
 			this.game.takeShot (numPoints);
 
+			// retrieve the element
+			element = document.getElementById("target");
+
+			// -> removing the class
+			  element.classList.remove("shake");
+			  
+			  // -> triggering reflow /* The actual magic */
+			  // without this it wouldn't work. Try uncommenting the line and the transition won't be retriggered.
+			  // Oops! This won't work in strict mode. Thanks Felis Phasma!
+			  // element.offsetWidth = element.offsetWidth;
+			  // Do this instead:
+			  void element.offsetWidth;
+			  
+			  // -> and re-adding the class
+			  element.classList.add("shake");
+
 		},
 
 		undoShot: function (numPoints) {
